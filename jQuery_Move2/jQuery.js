fa825a2a195d7705ad40206a84e18312fe34687c
Abdyka <dycka48god@gmail.com>
2016-06-01 23:39:40 +0700

    edit html and jquery

--- jQuery_Move2/jQuery.js
+++ jQuery_Move2/jQuery.js
@@ -2,7 +2,7 @@ var main = function() {
   var cities = ['Tokyo', 'Seoul', 'Mexico City', 'New York', 'Mumbai', 'Jakarta', 'Sao Paulo', 'Delhi', 'Keihanshin', 'Shanghai', 'Manila', 'Hong Kong', 'Los Angeles', 'Kolkata', 'Moscow', 'Cairo', 'Buenos Aires', 'London', 'Beijing', 'Karachi', 'Paris'];
     cities.sort();
 
-    $("#searchBox").autocomplete({
+    $("#search").autocomplete({
         source: cities
     });
 };
